c8c6253e9a9f80bb0c20b000c29a05050412a1e1
Mohammed-Hamada <mohammed3rbio@gmail.com>
2022-10-09 20:38:11 +0300

    Add 'chmod' command

--- The missing semester of your CS education/The Shell/commands.sh
+++ The missing semester of your CS education/The Shell/commands.sh
@@ -3,16 +3,23 @@
 # >>    Append
 
 # echo command
-echo hello\ world                                 # Print hello world into terminal 
-echo ./hello.txt                                  # Print the content of "hello.txt" file into terminal 
-echo hello > ./hello.txt                          # Override "hello.txt" file with "echo" output
-echo hello >> ./hello.txt                         # Append the output of the echo command into "hello.txt"
+echo hello\ world        # Print hello world into terminal
+echo ./hello.txt         # Print the content of "hello.txt" file into terminal
+echo hello >./hello.txt  # Override "hello.txt" file with "echo" output
+echo hello >>./hello.txt # Append the output of the echo command into "hello.txt"
 
 # cat command
-cat ./hello.txt                                   # Print on the content of "hello.txt" file
-cat ./hello.txt .hello1.txt                       # Concatinate the files and print on the concatination output
-cat hello.txt > hello1.txt                        # Override "hello1.txt" file with the output of "cat hello.txt"
-cat hello.txt >> hello1.txt                       # Append the output of the "cat hello.txt" command into "hello1.txt"
-tail hello.txt -n1 | cat >> tail1-hello.txt       # Pipe the output of the first commnad to be the input of the second command
+cat ./hello.txt                            # Print on the content of "hello.txt" file
+cat ./hello.txt .hello1.txt                # Concatinate the files and print on the concatination output
+cat hello.txt >hello1.txt                  # Override "hello1.txt" file with the output of "cat hello.txt"
+cat hello.txt >>hello1.txt                 # Append the output of the "cat hello.txt" command into "hello1.txt"
+tail hello.txt -n1 | cat >>tail1-hello.txt # Pipe the output of the first commnad to be the input of the second command
 
 # chmod command
+# (u)         : user
+# (g)         : groub
+# (o)         : others
+# (a, ugo)    : all
+chmod hello.txt u+w  # Add "wirte" permission to the user
+chmod hello.txt g-wx # remove "write", "execute" permissions from the groub
+chmod hello.txt a=rw # Add "read", "write" permissions to all users and remove "execute" permission
