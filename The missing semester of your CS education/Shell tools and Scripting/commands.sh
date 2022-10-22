c9e7c58d65db650599a4f9002d228d2b7f2db775
Mohammed-Hamada <mohammed3rbio@gmail.com>
2022-10-22 16:08:04 +0300

    Example for shell scripting and using the special variables

---
+++ The missing semester of your CS education/Shell tools and Scripting/commands.sh
@@ -0,0 +1,8 @@
+# Define a variable
+foo=bar
+echo $foo # bar
+
+foo = bar # Won't work, space after word means this word is (command, program)
+##########################
+echo "The value for foo is $foo" # The value for foo is bar
+# foobar
